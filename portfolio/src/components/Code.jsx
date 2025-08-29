98c521d211d076a3e7b9f9e9ca6092edabc5211e
SGORGONE <sebgorgone@gmail.com>
2025-08-29 15:22:42 -0400

    more content changes and button div

--- portfolio/src/components/Code.jsx
+++ portfolio/src/components/Code.jsx
@@ -27,11 +27,21 @@ function Code (props) {
          </div>
 
          <h1 style={{ textAlignLast: "center", fontFamily: "main", color: (colorMode ? "#6d3b47" : "#21263b"), fontSize: "10vw"}}>Projects</h1>
+
+         <div style={{display: "flex", justifyContent: "space-around", alignItems: "center", marginLeft: "5em", marginRight: "5em",marginBottom: "2em"}}>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+         </div>
+
+
+
          <div>
 
             <div style={{display: "flex", justifyContent: "center", alignItems: "center", marginLeft: "5em", marginRight: "5em", flexDirection: "column"}}>
-               <p style={{textAlign: "center", color: "white", fontFamily: "subMain"}}>Cloudlog is my CRUD project app that I made over the summer of 2025. I built it with the intention of learning the react framework, and in turn built the react frontend. I also built the backend in node.js with express.js as my API layer. I used MYSQL for the database and did my best to incorperate as much relational data queries as possible. I implemented my own auth using JSON web tokens for session validation.</p>
-               <p style={{textAlign: "center", color: "white", fontFamily: "subMain"}}>This rendition of the app is meant to work with the local storage in the browser for display. The original app was hosted fully on AWS. I used AWS Amplify for the frontend, lambda functions for the backend, and an ec2 instance for the mysql database.</p>
+               <p style={{textAlign: "center", color: "white", fontFamily: "subMain"}}>This is CloudLog! A skydiving logbook app aimed to deliver stats and retrospectives to skydivers. Cloudlog is my CRUD project app that I made over the summer of 2025. I built it with the intention of learning the react framework, and in turn built out the frontend in react. I built the backend in node.js with express.js as my API layer. I used MYSQL for the database and did my best to incorperate as much relational data queries as possible. I implemented my own auth using JSON web tokens for session validation.</p>
+               <p style={{textAlign: "center", color: "white", fontFamily: "subMain"}}>This rendition of the app is meant to work with the local storage in the browser for display. The original app was hosted fully on AWS. I used AWS Amplify for the frontend, lambda functions for the backend, and an ec2 instance for the mysql database. Im still in the process of converting the api endpoints to store data in local caches.</p>
                <p style={{textAlign: "center", color: "white", fontFamily: "subMain"}}>check out the frontend published with github pages, or the original fullstack project repo! (links underneath)</p>
 
 
@@ -43,6 +53,13 @@ function Code (props) {
                   style={{width: "75%", height: "50vw", border: `solid .25em ${(colorMode ? "#6d3b47" : "#2f4858")}`, boxShadow: `3px 3px 6px rgba(0, 0, 0, 0.15), 10px 10px 20px rgba(0, 0, 0, 0.1), 15px 15px 30px rgba(0, 0, 0, 0.05)`, borderRadius: ".5em"}}
                />
             </div>
+
+            <div style={{display: "flex", flexDirection: "column",margin: "1em", marginLeft: "5em", marginRight: "5em"}}>
+               <a style={{fontFamily: "subMain", color: "white", cursor: "pointer"}} href='https://github.com/sebgorgone/Cloud-Log' title="cloudlog repo" target='none'>Cloudlog repo</a>
+               <a style={{fontFamily: "subMain", color: "white", cursor: "pointer"}} href='https://sebgorgone.github.io/cloudlog-local/#/login' title="cloudlog repo" target='none'>Cloudlog UI display</a>
+            </div>
+
+
          </div>
 
       </div>
